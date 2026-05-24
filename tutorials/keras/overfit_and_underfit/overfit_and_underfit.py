4307311142731d38554c8f386907dd42ce980ac7
Florent BONNIN <florent-bonnin@live.fr>
2026-05-24 22:55:25 +0200

    Overfit and underfit

---
+++ tutorials/keras/overfit_and_underfit/overfit_and_underfit.py
@@ -0,0 +1,223 @@
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+import tensorflow as tf
+
+from tensorflow.keras import layers
+from tensorflow.keras import regularizers
+
+print(tf.__version__)
+
+import tensorflow_docs as tfdocs
+import tensorflow_docs.modeling
+import tensorflow_docs.plots
+
+from IPython import display
+from matplotlib import pyplot as plt
+
+import numpy as np
+
+import pathlib
+import shutil
+import tempfile
+
+logdir = pathlib.Path(tempfile.mkdtemp())/"tensorboard_logs"
+shutil.rmtree(logdir, ignore_errors=True)
+
+gz = tf.keras.utils.get_file('HIGGS.csv.gz', 'http://mlphysics.ics.uci.edu/data/higgs/HIGGS.csv.gz')
+
+FEATURES = 28
+
+ds = tf.data.experimental.CsvDataset(gz,[float(),]*(FEATURES+1), compression_type="GZIP")
+
+def pack_row(*row):
+  label = row[0]
+  features = tf.stack(row[1:],1)
+  return features, label
+
+packed_ds = ds.batch(10000).map(pack_row).unbatch()
+
+for features,label in packed_ds.batch(1000).take(1):
+  print(features[0])
+  plt.figure()
+  plt.hist(features.numpy().flatten(), bins = 101)
+  plt.savefig("1.png")
+
+N_VALIDATION = int(1e3)
+N_TRAIN = int(1e4)
+BUFFER_SIZE = int(1e4)
+BATCH_SIZE = 500
+STEPS_PER_EPOCH = N_TRAIN//BATCH_SIZE
+
+validate_ds = packed_ds.take(N_VALIDATION).cache()
+train_ds = packed_ds.skip(N_VALIDATION).take(N_TRAIN).cache()
+
+print(train_ds)
+
+validate_ds = validate_ds.batch(BATCH_SIZE)
+train_ds = train_ds.shuffle(BUFFER_SIZE).repeat().batch(BATCH_SIZE)
+
+lr_schedule = tf.keras.optimizers.schedules.InverseTimeDecay(
+  0.001,
+  decay_steps=STEPS_PER_EPOCH*1000,
+  decay_rate=1,
+  staircase=False)
+
+def get_optimizer():
+  return tf.keras.optimizers.Adam(lr_schedule)
+
+step = np.linspace(0,100000)
+lr = lr_schedule(step)
+plt.figure(figsize = (8,6))
+plt.plot(step/STEPS_PER_EPOCH, lr)
+plt.ylim([0,max(plt.ylim())])
+plt.xlabel('Epoch')
+_ = plt.ylabel('Learning Rate')
+plt.savefig("2.png")
+
+def get_callbacks(name):
+  return [
+    tfdocs.modeling.EpochDots(),
+    tf.keras.callbacks.EarlyStopping(monitor='val_binary_crossentropy', patience=200),
+    tf.keras.callbacks.TensorBoard(logdir/name),
+  ]
+
+def compile_and_fit(model, name, optimizer=None, max_epochs=10000):
+  if optimizer is None:
+    optimizer = get_optimizer()
+  model.compile(optimizer=optimizer,
+                loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                metrics=[
+                  tf.keras.metrics.BinaryCrossentropy(
+                      from_logits=True, name='binary_crossentropy'),
+                  'accuracy'])
+
+  model.summary()
+
+  history = model.fit(
+    train_ds.map(lambda x, y: (x, tf.expand_dims(y, axis=-1))),
+    steps_per_epoch = STEPS_PER_EPOCH,
+    epochs=max_epochs,
+    validation_data=validate_ds.map(lambda x, y: (x, tf.expand_dims(y, axis=-1))),
+    callbacks=get_callbacks(name),
+    verbose=0)
+  return history
+
+tiny_model = tf.keras.Sequential([
+    layers.Dense(16, activation='elu', input_shape=(FEATURES,)),
+    layers.Dense(1)
+])
+
+size_histories = {}
+
+size_histories['Tiny'] = compile_and_fit(tiny_model, 'sizes/Tiny')
+
+plotter = tfdocs.plots.HistoryPlotter(metric = 'binary_crossentropy', smoothing_std=10)
+plt.figure()
+plotter.plot(size_histories)
+plt.ylim([0.5, 0.7])
+plt.savefig("3.png")
+
+small_model = tf.keras.Sequential([
+    # `input_shape` is only required here so that `.summary` works.
+    layers.Dense(16, activation='elu', input_shape=(FEATURES,)),
+    layers.Dense(16, activation='elu'),
+    layers.Dense(1)
+])
+
+size_histories['Small'] = compile_and_fit(small_model, 'sizes/Small')
+
+medium_model = tf.keras.Sequential([
+    layers.Dense(64, activation='elu', input_shape=(FEATURES,)),
+    layers.Dense(64, activation='elu'),
+    layers.Dense(64, activation='elu'),
+    layers.Dense(1)
+])
+
+size_histories['Medium'] = compile_and_fit(medium_model, "sizes/Medium")
+
+large_model = tf.keras.Sequential([
+    layers.Dense(512, activation='elu', input_shape=(FEATURES,)),
+    layers.Dense(512, activation='elu'),
+    layers.Dense(512, activation='elu'),
+    layers.Dense(512, activation='elu'),
+    layers.Dense(1)
+])
+
+size_histories['large'] = compile_and_fit(large_model, "sizes/large")
+
+plt.figure()
+plotter.plot(size_histories)
+a = plt.xscale('log')
+plt.xlim([5, max(plt.xlim())])
+plt.ylim([0.5, 0.7])
+plt.xlabel("Epochs [Log Scale]")
+plt.savefig("4.png")
+
+shutil.rmtree(logdir/'regularizers/Tiny', ignore_errors=True)
+shutil.copytree(logdir/'sizes/Tiny', logdir/'regularizers/Tiny')
+
+regularizer_histories = {}
+regularizer_histories['Tiny'] = size_histories['Tiny']
+
+l2_model = tf.keras.Sequential([
+    layers.Dense(512, activation='elu',
+                 kernel_regularizer=regularizers.l2(0.001),
+                 input_shape=(FEATURES,)),
+    layers.Dense(512, activation='elu',
+                 kernel_regularizer=regularizers.l2(0.001)),
+    layers.Dense(512, activation='elu',
+                 kernel_regularizer=regularizers.l2(0.001)),
+    layers.Dense(512, activation='elu',
+                 kernel_regularizer=regularizers.l2(0.001)),
+    layers.Dense(1)
+])
+
+regularizer_histories['l2'] = compile_and_fit(l2_model, "regularizers/l2")
+
+plt.figure()
+plotter.plot(regularizer_histories)
+plt.ylim([0.5, 0.7])
+plt.savefig("5.png")
+
+dropout_model = tf.keras.Sequential([
+    layers.Dense(512, activation='elu', input_shape=(FEATURES,)),
+    layers.Dropout(0.5),
+    layers.Dense(512, activation='elu'),
+    layers.Dropout(0.5),
+    layers.Dense(512, activation='elu'),
+    layers.Dropout(0.5),
+    layers.Dense(512, activation='elu'),
+    layers.Dropout(0.5),
+    layers.Dense(1)
+])
+
+regularizer_histories['dropout'] = compile_and_fit(dropout_model, "regularizers/dropout")
+
+plt.figure()
+plotter.plot(regularizer_histories)
+plt.ylim([0.5, 0.7])
+plt.savefig("6.png")
+
+combined_model = tf.keras.Sequential([
+    layers.Dense(512, kernel_regularizer=regularizers.l2(0.0001),
+                 activation='elu', input_shape=(FEATURES,)),
+    layers.Dropout(0.5),
+    layers.Dense(512, kernel_regularizer=regularizers.l2(0.0001),
+                 activation='elu'),
+    layers.Dropout(0.5),
+    layers.Dense(512, kernel_regularizer=regularizers.l2(0.0001),
+                 activation='elu'),
+    layers.Dropout(0.5),
+    layers.Dense(512, kernel_regularizer=regularizers.l2(0.0001),
+                 activation='elu'),
+    layers.Dropout(0.5),
+    layers.Dense(1)
+])
+
+regularizer_histories['combined'] = compile_and_fit(combined_model, "regularizers/combined")
+
+plt.figure()
+plotter.plot(regularizer_histories)
+plt.ylim([0.5, 0.7])
+plt.savefig("7.png")
