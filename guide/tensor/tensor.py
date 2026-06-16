1acf646239894f7e0a6a2e899687979e09c5140e
Florent BONNIN <florent-bonnin@live.fr>
2026-06-16 21:39:11 +0200

    Tensors

---
+++ guide/tensor/tensor.py
@@ -0,0 +1,234 @@
+import tensorflow as tf
+import numpy as np
+
+# This will be an int32 tensor by default; see "dtypes" below.
+rank_0_tensor = tf.constant(4)
+print(rank_0_tensor)
+
+# Let's make this a float tensor.
+rank_1_tensor = tf.constant([2.0, 3.0, 4.0])
+print(rank_1_tensor)
+
+# If you want to be specific, you can set the dtype (see below) at creation time
+rank_2_tensor = tf.constant([[1, 2],
+                             [3, 4],
+                             [5, 6]], dtype=tf.float16)
+print(rank_2_tensor)
+
+# There can be an arbitrary number of
+# axes (sometimes called "dimensions")
+rank_3_tensor = tf.constant([
+  [[0, 1, 2, 3, 4],
+   [5, 6, 7, 8, 9]],
+  [[10, 11, 12, 13, 14],
+   [15, 16, 17, 18, 19]],
+  [[20, 21, 22, 23, 24],
+   [25, 26, 27, 28, 29]],])
+
+print(rank_3_tensor)
+
+print(np.array(rank_2_tensor))
+
+print(rank_2_tensor.numpy())
+
+a = tf.constant([[1, 2],
+                 [3, 4]])
+b = tf.constant([[1, 1],
+                 [1, 1]]) # Could have also said `tf.ones([2,2], dtype=tf.int32)`
+
+print(tf.add(a, b), "\n")
+print(tf.multiply(a, b), "\n")
+print(tf.matmul(a, b), "\n")
+
+print(a + b, "\n") # element-wise addition
+print(a * b, "\n") # element-wise multiplication
+print(a @ b, "\n") # matrix multiplication
+
+c = tf.constant([[4.0, 5.0], [10.0, 1.0]])
+
+# Find the largest value
+print(tf.reduce_max(c))
+# Find the index of the largest value
+print(tf.math.argmax(c))
+# Compute the softmax
+print(tf.nn.softmax(c))
+
+print(tf.convert_to_tensor([1,2,3]))
+
+print(tf.reduce_max([1,2,3]))
+
+print(tf.reduce_max(np.array([1,2,3])))
+
+rank_4_tensor = tf.zeros([3, 2, 4, 5])
+
+print("Type of every element:", rank_4_tensor.dtype)
+print("Number of axes:", rank_4_tensor.ndim)
+print("Shape of tensor:", rank_4_tensor.shape)
+print("Elements along axis 0 of tensor:", rank_4_tensor.shape[0])
+print("Elements along the last axis of tensor:", rank_4_tensor.shape[-1])
+print("Total number of elements (3*2*4*5): ", tf.size(rank_4_tensor).numpy())
+
+print(tf.rank(rank_4_tensor))
+
+print(tf.shape(rank_4_tensor))
+
+rank_1_tensor = tf.constant([0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
+print(rank_1_tensor.numpy())
+
+print("First:", rank_1_tensor[0].numpy())
+print("Second:", rank_1_tensor[1].numpy())
+print("Last:", rank_1_tensor[-1].numpy())
+
+print("Everything:", rank_1_tensor[:].numpy())
+print("Before 4:", rank_1_tensor[:4].numpy())
+print("From 4 to the end:", rank_1_tensor[4:].numpy())
+print("From 2, before 7:", rank_1_tensor[2:7].numpy())
+print("Every other item:", rank_1_tensor[::2].numpy())
+print("Reversed:", rank_1_tensor[::-1].numpy())
+
+print(rank_2_tensor.numpy())
+
+# Pull out a single value from a 2-rank tensor
+print(rank_2_tensor[1, 1].numpy())
+
+# Get row and column tensors
+print("Second row:", rank_2_tensor[1, :].numpy())
+print("Second column:", rank_2_tensor[:, 1].numpy())
+print("Last row:", rank_2_tensor[-1, :].numpy())
+print("First item in last column:", rank_2_tensor[0, -1].numpy())
+print("Skip the first row:")
+print(rank_2_tensor[1:, :].numpy(), "\n")
+
+print(rank_3_tensor[:, :, 4])
+
+# Shape returns a `TensorShape` object that shows the size along each axis
+x = tf.constant([[1], [2], [2]])
+print(x.shape)
+
+# You can convert this object into a Python list, too
+print(x.shape.as_list())
+
+# You can reshape a tensor to a new shape.
+# Note that you're passing in a list
+reshaped = tf.reshape(x, [1, 3])
+
+print(x.shape)
+print(reshaped.shape)
+
+print(rank_3_tensor)
+
+# A `-1` passed in the `shape` argument says "Whatever fits".
+print(tf.reshape(rank_3_tensor, [-1]))
+
+print(tf.reshape(rank_3_tensor, [3*2, 5]), "\n")
+print(tf.reshape(rank_3_tensor, [3, -1]))
+
+# Bad examples: don't do this
+
+# You can't reorder axes with reshape.
+print(tf.reshape(rank_3_tensor, [2, 3, 5]), "\n")
+
+# This is a mess
+print(tf.reshape(rank_3_tensor, [5, 6]), "\n")
+
+# This doesn't work at all
+try:
+  tf.reshape(rank_3_tensor, [7, -1])
+except Exception as e:
+  print(f"{type(e).__name__}: {e}")
+
+the_f64_tensor = tf.constant([2.2, 3.3, 4.4], dtype=tf.float64)
+the_f16_tensor = tf.cast(the_f64_tensor, dtype=tf.float16)
+# Now, cast to an uint8 and lose the decimal precision
+the_u8_tensor = tf.cast(the_f16_tensor, dtype=tf.uint8)
+print(the_u8_tensor)
+
+x = tf.constant([1, 2, 3])
+
+y = tf.constant(2)
+z = tf.constant([2, 2, 2])
+# All of these are the same computation
+print(tf.multiply(x, 2))
+print(x * y)
+print(x * z)
+
+# These are the same computations
+x = tf.reshape(x,[3,1])
+y = tf.range(1, 5)
+print(x, "\n")
+print(y, "\n")
+print(tf.multiply(x, y))
+
+x_stretch = tf.constant([[1, 1, 1, 1],
+                         [2, 2, 2, 2],
+                         [3, 3, 3, 3]])
+
+y_stretch = tf.constant([[1, 2, 3, 4],
+                         [1, 2, 3, 4],
+                         [1, 2, 3, 4]])
+
+print(x_stretch * y_stretch)  # Again, operator overloading
+
+print(tf.broadcast_to(tf.constant([1, 2, 3]), [3, 3]))
+
+ragged_list = [
+    [0, 1, 2, 3],
+    [4, 5],
+    [6, 7, 8],
+    [9]]
+
+try:
+  tensor = tf.constant(ragged_list)
+except Exception as e:
+  print(f"{type(e).__name__}: {e}")
+
+ragged_tensor = tf.ragged.constant(ragged_list)
+print(ragged_tensor)
+
+print(ragged_tensor.shape)
+
+# Tensors can be strings, too here is a scalar string.
+scalar_string_tensor = tf.constant("Gray wolf")
+print(scalar_string_tensor)
+
+# If you have three string tensors of different lengths, this is OK.
+tensor_of_strings = tf.constant(["Gray wolf",
+                                 "Quick brown fox",
+                                 "Lazy dog"])
+# Note that the shape is (3,). The string length is not included.
+print(tensor_of_strings)
+
+print(tf.constant("🥳👍"))
+
+# You can use split to split a string into a set of tensors
+print(tf.strings.split(scalar_string_tensor, sep=" "))
+
+# ...but it turns into a `RaggedTensor` if you split up a tensor of strings,
+# as each string might be split into a different number of parts.
+print(tf.strings.split(tensor_of_strings))
+
+text = tf.constant("1 10 100")
+print(tf.strings.to_number(tf.strings.split(text, " ")))
+
+byte_strings = tf.strings.bytes_split(tf.constant("Duck"))
+byte_ints = tf.io.decode_raw(tf.constant("Duck"), tf.uint8)
+print("Byte strings:", byte_strings)
+print("Bytes:", byte_ints)
+
+# Or split it up as unicode and then decode it
+unicode_bytes = tf.constant("アヒル 🦆")
+unicode_char_bytes = tf.strings.unicode_split(unicode_bytes, "UTF-8")
+unicode_values = tf.strings.unicode_decode(unicode_bytes, "UTF-8")
+
+print("\nUnicode bytes:", unicode_bytes)
+print("\nUnicode chars:", unicode_char_bytes)
+print("\nUnicode values:", unicode_values)
+
+# Sparse tensors store values by index in a memory-efficient manner
+sparse_tensor = tf.sparse.SparseTensor(indices=[[0, 0], [1, 2]],
+                                       values=[1, 2],
+                                       dense_shape=[3, 4])
+print(sparse_tensor, "\n")
+
+# You can convert sparse tensors to dense
+print(tf.sparse.to_dense(sparse_tensor))
